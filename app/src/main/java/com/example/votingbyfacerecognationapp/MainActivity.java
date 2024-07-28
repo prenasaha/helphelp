54af225d452e6eca8df5cabc731c0ee475aa1d5a
prena saha <saha2017sajal@gmail.com>
2024-07-28 13:47:12 +0530

    first commit

--- app/src/main/java/com/example/votingbyfacerecognationapp/MainActivity.java
+++ app/src/main/java/com/example/votingbyfacerecognationapp/MainActivity.java
@@ -5,20 +5,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    private TextView textview;
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener()
+        textview= findViewById(R.id.textView);
+         button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
